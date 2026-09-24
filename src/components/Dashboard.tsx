9a96bafb6559baecca49561b10bf24ea52498b34
hds05 <duttsharmahimanshu96@gmail.com>
2026-09-24 11:31:58 +0530

    feat: enhance UI of header and add profile image

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Dashboard() {
   return (
-    <div className='flex justify-evenly gap-4'>
-        <div className='p-4 text-amber-700 font-mono border rounded-4xl w-[300px] text-center cursor-pointer transition transform hover:scale-115 bg-black hover:z-50 hover:shadow-[1px_2px_6px_white]'>Mental Math</div>
-        <div className='p-4 text-amber-700 font-mono border rounded-4xl w-[300px] text-center cursor-pointer transition transform hover:scale-115 bg-black hover:z-50 hover:shadow-[1px_2px_6px_white]'>Train your Calculation Speed.</div>
+    <div className="flex flex-col lg:flex-row justify-evenly gap-4">
+      <div className="p-4 text-amber-400 font-mono border rounded-4xl w-[300px] text-center cursor-pointer transition transform hover:scale-105 bg-black hover:z-50 hover:shadow-[0px_0px_6px_purple] hover:text-white">
+        Mental Math
+      </div>
+      <div className="p-4 text-amber-400 font-mono border rounded-4xl w-[300px] text-center cursor-pointer transition transform hover:scale-105 bg-black hover:z-50 hover:shadow-[0px_0px_6px_purple] hover:text-white">
+        Train your Calculation Speed.
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  const [open, setOpen] = useState(false);
+  function openMenu() {
+    setOpen((prev) => !prev);
+  }
   return (
-    <div className=" flex justify-between p-4 shadow-[0px_1px_50px_white]">
-      <h1 className="text-white font-extrabold">Kalculas</h1>
-      <div>
-        <button></button>
+    <div className=" flex justify-between items-center bg-black p-4 rounded-[0_0_20px_20px]">
+      {/* <h1 className="text-white font-extrabold">Kalculas</h1> */}
+      {/* <img src="./logo-1.png" width={"90px"} className="rounded" alt="" /> */}
+      <div className="my-1 flex items-center gap-4">
+        <img src="kalculas_icon_logo.png" width={"40px"} alt="" />
+        <h1 className="text-white text-xl font-mono">Kalculas</h1>
+      </div>
+      <div className="text-white flex gap-3 bg-[#16161D] rounded-3xl p-2 justify-between">
+        <button className="hover:bg-gray-400/30 cursor-pointer p-1 rounded text-sm">Practice</button>
+        <button className="hover:bg-gray-400/30 cursor-pointer p-1 rounded text-sm">Progress</button>
+        <button className="hover:bg-gray-400/30 cursor-pointer p-1 rounded text-sm">Setting</button>
+      </div>
+      <div className="relative flex gap-3">
+        <div className="text-kal-gold text-xs flex items-center justify-center bg-kal-gold-light/20 border rounded-4xl px-4">
+          Streak
+        </div>
+        <div
+          onClick={() => openMenu()}
+          className="text-white cursor-pointer rounded-full overflow-hidden"
+        >
+          <img src="/Profile_image.png" width={'30px'} alt="" />
+        </div>
+        {open && (
+          <div className="absolute top-15 right-2 bg-white text-black p-4 rounded-2xl">
+            <ul>
+              <li>Home</li>
+              <li>Dashboard</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
