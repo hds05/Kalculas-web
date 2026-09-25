f88160a34883cc512e59f6190c92be026ef017d4
hds05 <duttsharmahimanshu96@gmail.com>
2026-09-25 15:49:48 +0530

    feat: update App component structure and enhance body styling with gradients

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
-import Setup from "./components/Page-2.tsx/Setup";
+import Setup from "./components/Page-2/Setup";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-black/85">
+    <div className="w-full h-screen ">
       <Header />
-      {/* <div className="flex justify-center items-center h-screen"> */}
-        {/* <Dashboard /> */}
-      {/* </div> */}
-        <Setup />
+      <div className="">
+        <Dashboard />
+      </div>
+        {/* <Setup /> */}
     </div>
   );
 }
